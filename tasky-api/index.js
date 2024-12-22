5f8173b8b358ce68b420f051930ceee446688347
Pav20097569 <20097569@mail.wit.ie>
2024-12-22 13:50:11 +0000

    Mongo Lab Users Router

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
+import usersRouter from './api/users';
+
 
 dotenv.config();
 
@@ -20,6 +22,8 @@ const port = process.env.PORT;
 app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
+
+app.use('/api/users', usersRouter);
 
 app.use(errHandler);
 
